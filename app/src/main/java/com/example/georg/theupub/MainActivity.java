10e0e76c1e5a911faeec2c86502e05c6c0515f39
gdemos01 <georgedemo2000@gmail.com>
2015-11-09 19:14:58 +0200

    Changes

--- app/src/main/java/com/example/georg/theupub/MainActivity.java
+++ app/src/main/java/com/example/georg/theupub/MainActivity.java
@@ -11,6 +11,7 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     String imadeachange="see my change!!!!";
+    //This is a change comment
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
