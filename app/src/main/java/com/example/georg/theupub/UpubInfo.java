693dd86397f7859df6ceec566d9cee04a102af66
Kyriakoszantis <Kyriakoszantis@gmail.com>
2015-11-14 03:55:39 +0200

    Editing Info activity  (colors) Change Icon

--- app/src/main/java/com/example/georg/theupub/UpubInfo.java
+++ app/src/main/java/com/example/georg/theupub/UpubInfo.java
@@ -23,14 +23,6 @@ public class UpubInfo extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,17 +72,13 @@ public class UpubInfo extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camara) {
+        if (id == R.id.nav_Info) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_Menu) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_Offers) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_Events) {
 
         }
 
