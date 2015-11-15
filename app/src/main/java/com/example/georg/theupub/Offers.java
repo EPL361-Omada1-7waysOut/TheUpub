818dab8dfc91f81c40b82c6d5208089f9bdfe13f
Rafaelmichael10 <rafaelapoel@hotmail.com>
2015-11-15 16:03:47 +0200

    Testing github
    Testing github entering spaces in offers.java

--- app/src/main/java/com/example/georg/theupub/Offers.java
+++ app/src/main/java/com/example/georg/theupub/Offers.java
@@ -12,6 +12,11 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+
+
+
+
+
 import android.view.MenuItem;
 
 public class Offers extends AppCompatActivity
