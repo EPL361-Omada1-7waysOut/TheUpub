fd1dd21fdd2fa2e22cf5dd4e22677edb2bca82b0
Rafaelmichael10 <rafaelapoel@hotmail.com>
2015-11-20 13:02:40 +0200

    Menu activity completed

--- app/src/main/java/com/example/georg/theupub/TheMenu.java
+++ app/src/main/java/com/example/georg/theupub/TheMenu.java
@@ -23,7 +23,7 @@ public class TheMenu extends AppCompatActivity
     String[] Titles;
     String[] Descriptions;
 
-    //List of images to be put in the listt
+    //List of images to be put in the list
     int [] images={
             R.drawable.keo,
             R.drawable.cas,
